89d1705dad61f6d5bbcb868c2d56c6d7ff555806
Khashan <simonandersonl@hotmail.com>
2019-09-21 17:08:15 -0400

    First commit

---
+++ Assets/PoolManager/Example/Scripts/PooledPrefabObjectTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooledPrefabObjectTest : PooledObject
+{
+    [SerializeField]
+    private bool m_AutoDisable = false;
+    [SerializeField]
+    private float m_LifeTime = -1f;
+
+    private float m_CurrentLifeTime = 0f;
+
+    private void OnEnable()
+    {
+        m_CurrentLifeTime = m_LifeTime;
+    }
+
+    private void Update()
+    {
+        if(m_AutoDisable)
+        {
+            m_CurrentLifeTime -= Time.deltaTime;
+            if(m_CurrentLifeTime <= 0)
+            {
+                Destroy(gameObject); //Never use Destroy! Use SetActive(False), I'm using Destroy to add a safety on the code
+                //gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/PoolManager/PooledObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public abstract class PooledObject : MonoBehaviour
+{
+    [SerializeField]
+    private bool m_DestroyErrorSourceOnly = false;
+
+    private GameObject m_PoolOwner;
+    public GameObject PoolOwner
+    {
+        get { return m_PoolOwner; }
+    }
+
+    private string m_StackTrace;
+
+    public virtual void InitPooledObject(GameObject a_PoolOwner)
+    {
+        m_PoolOwner = a_PoolOwner;
+        gameObject.SetActive(false);
+    }
+
+    protected virtual void OnDisable()
+    {
+        #if UNITY_EDITOR
+        m_StackTrace = StackTraceUtility.ExtractStackTrace();
+        #endif
+
+        PoolManager.Instance.ReturnedPooledObject(this, PoolOwner);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        #if UNITY_EDITOR
+        Debug.LogError(GetDestroyErrorMessage());
+        #endif
+    }
+
+    #if UNITY_EDITOR
+    private string GetDestroyErrorMessage()
+    {
+        string errorMessage = "PooledObject should've never been destroyed! \n{0}";
+
+        if(!m_DestroyErrorSourceOnly)
+        {
+            errorMessage = string.Format(errorMessage, m_StackTrace);
+        }
+        else
+        {
+            errorMessage = string.Format(errorMessage, GetDestroyerSource());
+        }
+
+        return errorMessage;
+    }
+
+    private string GetDestroyerSource()
+    {
+        string[] traces = m_StackTrace.Split('\n');
+        return traces[traces.Length - 2] + "\n";
+    }
+    #endif
+}
